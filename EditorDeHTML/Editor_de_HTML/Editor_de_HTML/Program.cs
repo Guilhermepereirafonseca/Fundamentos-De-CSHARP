3fcadd5e257e5b879c78a487258f0175db9cfd2b
Guilhermepereirafonseca <guilhermepereirafonseca305@gmail.com>
2025-02-26 15:55:20 -0300

    Add Project Editor de HTML
    26/02/2025 as 15:55

---
+++ EditorDeHTML/Editor_de_HTML/Editor_de_HTML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Editor_de_HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
---
+++ EditorDeHTML/Editor_de_HTML/Editor_de_HTML/Viewer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace Editor_de_HTML
+{
+    public class Viewer
+    {
+        public static void Show(string text)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO VISUALIZAÇÃO");
+            Console.WriteLine("------------");
+            Replace(text);
+            Console.WriteLine("------------");
+            Console.ReadKey();
+            Menu.Show();
+        }
+
+        public static void Replace(string text)
+        {
+            var strong = new Regex(@"\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            var words = text.Split(' '); // pega todas as palavras do texto
+
+            for(var i = 0; i < words.Length;  i++)
+            {
+                if(strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(words[i].Substring(words[i].IndexOf('>') + 1));
+                }
+            }
+        }
+    }
+}
